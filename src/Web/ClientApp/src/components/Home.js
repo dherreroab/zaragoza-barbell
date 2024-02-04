2c826d0ef6f0a85754f0c25a1cf0a84f4edb2de3
dherreroab <142175506+dherreroab@users.noreply.github.com>
2024-02-04 03:34:17 +0100

    Add videos and adjust slides (#4)

--- src/Web/ClientApp/src/components/Home.js
+++ src/Web/ClientApp/src/components/Home.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import CookiePolicy from './CookiePolicy';
 import Team from './Team';
-import zgzbarbell1 from '../assets/img/zgzb1.jpg';
+import img1 from '../assets/img/zgzb1.jpg';
+import img2 from '../assets/img/zgzb2.jpg';
+import img3 from '../assets/img/zgzb3.jpg';
+import video1 from '../assets/video/video1.mp4';
+import video2 from '../assets/video/video2.mp4';
+import video3 from '../assets/video/video3.mp4';
 import './styles/Home.css';
 
 
@@ -15,22 +20,46 @@ export class Home extends Component {
         <CookiePolicy />
         <div className="carousel" id="carousel">
           <Carousel>
+          <Carousel.Item>
+              <video className="d-block w-100 fullscreen-video" controls>
+                <source src={video1} type="video/mp4" />
+              </video>
+              <Carousel.Caption>
+                <h3>Video 1</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
             <Carousel.Item>
-              <img className="d-block w-100" src={zgzbarbell1} alt="First slide" />
+              <video className="d-block w-100 fullscreen-video" controls>
+                <source src={video2} type="video/mp4" />
+              </video>
+              <Carousel.Caption>
+                <h3>Video 2</h3>
+              </Carousel.Caption>
+              <Carousel.Item>
+              <video className="d-block w-100 fullscreen-video" controls>
+                <source src={video3} type="video/mp4" />
+              </video>
+              <Carousel.Caption>
+                <h3>Video 3</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={img1} alt="First slide" />
               <Carousel.Caption>
                 <h3>Zaragoza Barbell Club Movera</h3>
                 <p>El frio curte a las verdaderas personas.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="d-block w-100" src={zgzbarbell1} alt="Second slide" />
+              <img className="d-block w-100" src={img2} alt="Second slide" />
               <Carousel.Caption>
                 <h3>Zaragoza Barbell Club Movera</h3>
                 <p>El frio curte a las verdaderas personas.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img className="d-block w-100" src={zgzbarbell1} alt="Third slide" />
+              <img className="d-block w-100" src={img3} alt="Third slide" />
               <Carousel.Caption>
                 <h3>Zaragoza Barbell Club Movera</h3>
                 <p>El frio curte a las verdaderas personas.</p>
