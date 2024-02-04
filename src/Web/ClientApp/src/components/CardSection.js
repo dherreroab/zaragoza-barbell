c25fab719b8bb5a4d21de0d4e302a13fab7bf268
David <dherrero.abadia@outlook.com>
2024-02-04 02:41:55 +0100

    Font changed and fixed icons

--- src/Web/ClientApp/src/components/CardSection.js
+++ src/Web/ClientApp/src/components/CardSection.js
@@ -5,7 +5,6 @@ class CardSection extends Component {
         return (
             <>
                 <section className={`section_${this.props.sectionName}`}>
-                    {/* <h2 className={this.props.sectionName}>{this.props.sectionTitle}</h2> */}
                     <p className={`${this.props.sectionName}__text`}>
                         {this.props.sectionText}
                     </p>
