985901f0436cf11244cd678f0590eb96badd8367
David <dherrero.abadia@outlook.com>
2024-02-07 22:52:04 +0100

    add helmet and google analitycs

--- src/Web/ClientApp/src/App.js
+++ src/Web/ClientApp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import AppRoutes from './AppRoutes';
 import Layout from './components/Layout';
 import './custom.css';
@@ -9,14 +10,27 @@ export default class App extends Component {
 
   render() {
     return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
+      <>
+        <Helmet>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-F0FBKK2JJS"></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-F0FBKK2JJS');
+            `}
+          </script>
+        </Helmet>
+        <Layout>
+          <Routes>
+            {AppRoutes.map((route, index) => {
+              const { element, ...rest } = route;
+              return <Route key={index} {...rest} element={element} />;
+            })}
+          </Routes>
+        </Layout>
+      </>
     );
   }
 }
